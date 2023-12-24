5a1bdf0458535a17d565d55f23629e119a5ea41a
DaveJhaesonAlivio <97799276+DaveJhaesonAlivio@users.noreply.github.com>
2023-12-24 11:26:26 +0800

    formatted the hero component

--- mlproject/src/Components/hero.jsx
+++ mlproject/src/Components/hero.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import bgimg from '../Assets/bgimg.jpeg'
+import React from "react";
+import bgimg from "../Assets/bgimg.jpeg";
 
 const hero = () => {
-    return (
-        <div className='p-4'>
-                <div className='relative'>
-                        {/*Overlay*/}
-                        <div className='absolute w-full h-full text-gray-200 '>
-                                <h1 className="text-4xl font-bold text-white py-6 ml-8">Unleash the Power, Play the Future</h1>
-                        
-                        </div>
-             <img src={bgimg} />
-                </div>
+  return (
+    <div className="p-4">
+      <div className="relative">
+        <div className="absolute w-full h-full text-gray-200 ">
+          <h1 className="text-4xl font-bold text-white py-6 ml-8">
+            Unleash the Power, Play the Future
+          </h1>
         </div>
-    )
-}
+        <img src={bgimg} />
+      </div>
+    </div>
+  );
+};
 
-export default hero
+export default hero;
